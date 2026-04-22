b141898daf7e8e0a08e59bad71da61e05878c0de
Iara <tuemail@example.com>
2026-04-22 16:30:14 -0300

    Primer commit
    Main con integrantes

---
+++ TETRIS/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Apellido: Rodriguez, Iara Sol
+//DNI: 42247089
+//Usuario: iarasolro
+//Entrega: S�
+//
+//Apellido: Cuevas Cardozo, Alejandro
+//DNI: 41202088
+//Usuario: AlejandroCu
+//Entrega: S�
+//
+//Apellido: Morales Tito, Ivan
+//DNI: 39772619
+//Usuario: IvanmTito
+//Entrega: S�
+
+int main()
+    return 0;
+}
